0159053f54bca349cc33ddb1a899c1547eb74674
wu <91170827+jaxsann@users.noreply.github.com>
2022-02-24 17:27:09 +0800

    Add files via upload

---
+++ week01/week01-2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+///    int a;
+///    printf("使用者輸入一個數值: ");
+///    scanf("%d",&a);
+///    printf("你讀到了 %d\n",a);
+///    return 0;
+    char c;
+    printf("請輸入一推字: ");
+    scanf("%c",&c);
+    printf("你讀到了 %c\n",c);
+    return 0;
+}
